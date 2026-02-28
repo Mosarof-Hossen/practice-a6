bf54f0fd424e5c7a6267631ca0a39f09f31bbb03
Mosarof-Hossen <mmd58553@gmail.com>
2026-02-28 16:34:49 +0600

    our service and contact section added

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ const allPlantDisplay = (tree) => {
     createNewDiv.innerHTML = `
 
 
-         <div class="bg-white rounded-2xl ">
+         <div class="bg-white rounded-2xl pb-2">
               <img class="w-full h-[250px] rounded-t-2xl p-1" src="${card.image}" alt="">
               <div class="p-1 space-y-1">
               <h1 class="px-3 text-2xl font-semibold">${card.name}</h1>
-              <p class="px-3 ">${card.description.slice(0, 50) + "..."}</p>
+              <p class="px-3 ">${card.description.slice(0, 30) + "..."}</p>
               <div class="flex justify-between items-center p-1">
                 <button class="border-1 border-green-400  rounded-xl p-2">${card.category}</button>
                 <h2 class="text-2xl">$ ${card.price}</h2>
@@ -53,8 +53,8 @@ const displayCategoriesName = (categore) => {
 
   // new element create && append container
   const newDivBtn = document.createElement("div");
-  newDivBtn.innerHTML = `<h1 onclick="allPlantsTree()" class="text-base py-2 px-2 rounded-sm hover:bg-green-600 hover:text-white"> All Tree</h1>`;
-  categoreContainer.append(newDivBtn);
+  newDivBtn.innerHTML = `<h1 id="all-tree" onclick="allPlantsTree()" class="mt-2 text-base py-2 px-2 rounded-sm hover:bg-green-600 hover:text-white lesson-btn"> All Tree</h1>`;
+  categoreContainer.appendChild(newDivBtn);
   //   append end
 
   // loop every lesson
@@ -62,7 +62,7 @@ const displayCategoriesName = (categore) => {
     const btnNewDiv = document.createElement("div");
     btnNewDiv.innerHTML = `
     
-    <h1 onclick="categoriesProduct(${categoriesName.id})" class="text-base py-2 px-2 rounded-sm hover:bg-green-600 hover:text-white">${categoriesName.category_name}</h1>
+    <h1 id="categories-name${categoriesName.id}" onclick="categoriesProduct(${categoriesName.id})" class="text-base mt-1 py-2 px-2 rounded-sm hover:bg-green-600 hover:text-white lesson-btn">${categoriesName.category_name}</h1>
         `;
     categoreContainer.append(btnNewDiv);
   });
@@ -73,6 +73,26 @@ categoriesNameShow();
 const categoriesProduct = (id) => {
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
-  .then((res)=>res.json())
-  .then((data)=>allPlantDisplay(data.plants))
+    .then((res) => res.json())
+    .then((data) => {
+      
+      removeActiveBtn();
+     
+      const categoriesButton = document.getElementById(`categories-name${id}`);
+      categoriesButton.classList.add("active");
+
+      allPlantDisplay(data.plants);
+    });
+};
+
+// remove active class function setup
+
+const removeActiveBtn = () => {
+ 
+  
+  const lessoneButton = document.querySelectorAll(".lesson-btn");
+  lessoneButton.forEach((btn) => {
+    btn.classList.remove("active");
+    
+  });
 };
