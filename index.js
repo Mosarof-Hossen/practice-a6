1800eddd7359e8edf5fc454b0e4f5d44702e1434
Mosarof-Hossen <mmd58553@gmail.com>
2026-02-28 22:58:14 +0600

    add to cart click show cart section product name product price and total tk calculate

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const allPlantDisplay = (tree) => {
                 <button class="border-1 border-green-400  rounded-xl p-2">${card.category}</button>
                 <h2 class="text-2xl">$ ${card.price}</h2>
               </div>
-              <button class="rounded-full py-2 w-full bg-green-600 text-xl text-white">Add to cart</button>
+              <button onclick="addToCart(${card.price},'${card.name}')" class="rounded-full py-2 w-full bg-green-600 text-xl text-white">Add to cart</button>
               </div>
             </div>
 
@@ -46,16 +46,12 @@ const categoriesNameShow = () => {
 };
 // display categories name
 const displayCategoriesName = (categore) => {
-  // get the container and empty
   const categoreContainer = document.getElementById("categore-name");
   categoreContainer.innerHTML = "";
-  //   container empty end
 
-  // new element create && append container
   const newDivBtn = document.createElement("div");
-  newDivBtn.innerHTML = `<h1 id="all-tree" onclick="allPlantsTree()" class="mt-2 text-base py-2 px-2 rounded-sm hover:bg-green-600 hover:text-white lesson-btn"> All Tree</h1>`;
+  newDivBtn.innerHTML = `<h1 id="all-tree" onclick="allPlantsTreeee()" class=" lesson-btn mt-2 text-base py-2 px-2 rounded-sm hover:bg-green-600 hover:text-white "> All Tree</h1>`;
   categoreContainer.appendChild(newDivBtn);
-  //   append end
 
   // loop every lesson
   categore.forEach((categoriesName) => {
@@ -75,9 +71,8 @@ const categoriesProduct = (id) => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      
       removeActiveBtn();
-     
+
       const categoriesButton = document.getElementById(`categories-name${id}`);
       categoriesButton.classList.add("active");
 
@@ -88,11 +83,61 @@ const categoriesProduct = (id) => {
 // remove active class function setup
 
 const removeActiveBtn = () => {
- 
-  
-  const lessoneButton = document.querySelectorAll(".lesson-btn");
+  const allTreeN = document.getElementById("all-tree");
+  allTreeN.classList.add("active");
+
+  const lessoneButton = document.querySelectorAll(".active");
   lessoneButton.forEach((btn) => {
     btn.classList.remove("active");
-    
   });
+};
+
+const allPlantsTreeee = () => {
+  removeActiveBtn();
+
+  const allTreeBtn = document.getElementById("all-tree");
+  allTreeBtn.classList.add("active");
+
+  allPlantsTree();
+};
+
+// add to cart function
+const cartShowName = [];
+
+const addToCart = (price, name) => {
+  cartShowName.push({ price, name });
+  cartShow();
+};
+
+const cartShow = () => {
+  const cardHistory = document.getElementById("card-history");
+  cardHistory.innerHTML = "";
+
+  let total = 0;
+
+  
+
+  
+
+  cartShowName.forEach((cart) => {
+    total += cart.price;
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+
+    <div class="flex justify-between bg-green-100 items-center p-2 rounded-md">
+                <span class="space-y-1">
+                  <h1>${cart.name}</h1>
+                <p class="text-gray-500">${cart.price} <i class="fa-solid fa-x text-[10px]"></i> 1</p>
+                </span>
+                <i class="fa-solid fa-x text-base text-gray-400"></i>
+              </div>
+
+    `;
+    cardHistory.append(newDiv);
+  });
+
+  const newTotalDiv=document.createElement("div")
+  newTotalDiv.innerHTML=`<h1> Total :${total}</h1>`
+
+  cardHistory.append(newTotalDiv)
 };
